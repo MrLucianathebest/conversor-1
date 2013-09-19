717baf33295181d1450f01662ab1277cd5171c3a
vital-edu <vitaldu@gmail.com>
2013-09-18 23:00:25 -0300

    Tornando os métodos públicos

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -2,35 +2,35 @@ package conversor;
 
 public class ConversorTemperatura {
 	//Métodos
-	int celsiusParaFahrenheit(){
+	public int celsiusParaFahrenheit(){
 		return 9*40/5+32;
 	}
 
-	int celsiusParaFahrenheit(int valorCelsius){
+	public int celsiusParaFahrenheit(int valorCelsius){
 		return 9*valorCelsius/5+32;
 	}	
 
-	double celsiusParaFahrenheit(double valorCelsius){
+	public double celsiusParaFahrenheit(double valorCelsius){
 		return valorCelsius*1.8+32;
 	}
 
-	double celsiusParaKelvin(double valorCelsius){
+	public double celsiusParaKelvin(double valorCelsius){
 		return valorCelsius+273.15;
 	}	
 
-	double fahrenheitParaCelsius(double valorFahrenheit){
+	public double fahrenheitParaCelsius(double valorFahrenheit){
 		return (valorFahrenheit-32)/1.8 ;
 	}
 
-	double fahrenheitParaKelvin(double valorFahrenheit){
+	public double fahrenheitParaKelvin(double valorFahrenheit){
 		return ((valorFahrenheit-32)/1.8)+273.15;
 	}
 
-	double kelvinParaCelsius(double valorKelvin){
+	public double kelvinParaCelsius(double valorKelvin){
 		return valorKelvin-273.15;
 	}
 
-	double kelvinParaFahrenheit(double valorKelvin){
+	public double kelvinParaFahrenheit(double valorKelvin){
 		return (valorKelvin - 273.15)*1.8+32;
 	}	
 }
