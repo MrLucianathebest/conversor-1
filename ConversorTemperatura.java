d6ae0bce05d3b6047c611e4866c18af276af5217
vital-edu <vitaldu@gmail.com>
2013-09-18 23:05:35 -0300

    Formatando corretamente as fórmulas dos métodos

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -3,34 +3,34 @@ package conversor;
 public class ConversorTemperatura {
 	//Métodos
 	public int celsiusParaFahrenheit(){
-		return 9*40/5+32;
+		return 9 * 40 / 5 + 32;
 	}
 
 	public int celsiusParaFahrenheit(int valorCelsius){
-		return 9*valorCelsius/5+32;
+		return 9 * valorCelsius/5 + 32;
 	}	
 
 	public double celsiusParaFahrenheit(double valorCelsius){
-		return valorCelsius*1.8+32;
+		return valorCelsius * 1.8 + 32;
 	}
 
 	public double celsiusParaKelvin(double valorCelsius){
-		return valorCelsius+273.15;
+		return valorCelsius + 273.15;
 	}	
 
 	public double fahrenheitParaCelsius(double valorFahrenheit){
-		return (valorFahrenheit-32)/1.8 ;
+		return (valorFahrenheit - 32) / 1.8;
 	}
 
 	public double fahrenheitParaKelvin(double valorFahrenheit){
-		return ((valorFahrenheit-32)/1.8)+273.15;
+		return ((valorFahrenheit - 32) / 1.8) + 273.15;
 	}
 
 	public double kelvinParaCelsius(double valorKelvin){
-		return valorKelvin-273.15;
+		return valorKelvin - 273.15;
 	}
 
 	public double kelvinParaFahrenheit(double valorKelvin){
-		return (valorKelvin - 273.15)*1.8+32;
+		return (valorKelvin - 273.15) * 1.8 + 32;
 	}	
 }
