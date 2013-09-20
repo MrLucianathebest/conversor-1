99f52d07abc1b3136d0b6a725e3800cbf3678334
Eduardo Vital <vitaldu@gmail.com>
2013-09-20 19:24:17 -0300

    Alterando a visibilidade dos métodos para público

--- Conversor.java
+++ Conversor.java
@@ -1,35 +1,38 @@
+
+package conversor;
+
 public class ConversorTemperatura {
-		//Métodos
-		int celsiusParaFahrenheit(){
-			return 9*40/5+32;
-		}
+	//Métodos
+	int celsiusParaFahrenheit(){
+		return 9*40/5+32;
+	}
 
-		int celsiusParaFahrenheit(int valorCelsius){
-			return 9*valorCelsius/5+32;
-		}	
+	public int celsiusParaFahrenheit(int valorCelsius){
+		return 9*valorCelsius/5+32;
+	}	
 
-		double celsiusParaFahrenheit(double valorCelsius){
-			return valorCelsius*1.8+32;
-		}
+	public double celsiusParaFahrenheit(double valorCelsius){
+		return valorCelsius*1.8+32;
+	}
 
-		double celsiusParaKelvin(double valorCelsius){
-			return valorCelsius+273.15;
-		}	
+	public double celsiusParaKelvin(double valorCelsius){
+		return valorCelsius+273.15;
+	}	
 
-		double fahrenheitParaCelsius(double valorFahrenheit){
-			return (valorFahrenheit-32)/1.8 ;
-		}
+	public double fahrenheitParaCelsius(double valorFahrenheit){
+		return (valorFahrenheit-32)/1.8 ;
+	}
 
-		double fahrenheitParaKelvin(double valorFahrenheit){
-			return ((valorFahrenheit-32)/1.8)+273.15;
-		}
+	public double fahrenheitParaKelvin(double valorFahrenheit){
+		return ((valorFahrenheit-32)/1.8)+273.15;
+	}
 
 
-		double kelvinParaCelsius(double valorKelvin){
-			return valorKelvin-273.15;
-		}
+	public double kelvinParaCelsius(double valorKelvin){
+		return valorKelvin-273.15;
+	}
 
-		double kelvinParaFahrenheit(double valorKelvin){
-			return (valorKelvin - 273.15)*1.8+32;
-		}	
+	public double kelvinParaFahrenheit(double valorKelvin){
+		return (valorKelvin - 273.15)*1.8+32;
+	}
 }
